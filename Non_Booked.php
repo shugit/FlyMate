03ea97c19a57d8c1a458bb46051a4c2d5c7a2913
Shuwen Zhou <jacobc3@gmail.com>
2014-05-13 12:28:20 +0800

    123

---
+++ Non_Booked.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="100%" border="1">
+  <tr>
+    <td><p>&nbsp;</p>
+    <p>Please fill in following</p>
+    <form id="form1" name="form1" method="post">
+      <table width="100%" border="1">
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td width="28%"><label for="depart">Departure City:</label></td>
+          <td width="72%"><select name="depart" id="depart">
+            <option value="1" selected="selected">Beijing</option>
+            <option value="2">Shanghai</option>
+            <option value="3">GuangZhou</option>
+            <option value="0">Other</option>
+          </select></td>
+        </tr>
+        <tr>
+          <td><label for="destination2">Destination City:</label></td>
+          <td><select name="destination" id="destination">
+            <option value="1" selected="selected">Sydney</option>
+            <option value="2">Melbourne</option>
+            <option value="3">Brisbane</option>
+            <option value="4">Adeledale</option>
+            <option value="0">Other</option>
+          </select></td>
+        </tr>
+        <tr>
+          <td><label for="month">Month:</label></td>
+          <td><p>
+            <input type="month" name="month" id="month">
+            </p></td>
+        </tr>
+        <tr>
+          <td><label for="day_section2">Day Section:</label></td>
+          <td><select name="day_section" id="day_section">
+            <option value="1" selected="selected">Early of the month</option>
+            <option value="2">Middle of the month</option>
+            <option value="3">End of the monthd</option>
+            <option value="0">All</option>
+          </select></td>
+        </tr>
+        <tr>
+          <td><label for="company2">Prefer Flight Company:</label></td>
+          <td><input type="text" name="company" id="company"></td>
+        </tr>
+        <tr>
+          <td><label for="price_low">Price Low bound:</label></td>
+          <td><input type="text" name="price_low" id="price_low"></td>
+        </tr>
+        <tr>
+          <td><label for="price_high">Price High Bound:</label></td>
+          <td><input type="text" name="price_high" id="price_high"></td>
+        </tr>
+        <tr>
+          <td><label for="gender2">Your Gender:</label></td>
+          <td><select name="gender" id="gender">
+            <option value="0">Female</option>
+            <option value="1">Male</option>
+          </select></td>
+        </tr>
+        <tr>
+          <td><label for="qq">QQ:</label></td>
+          <td><input type="text" name="qq" id="qq"></td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+</table>
+    </form></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
